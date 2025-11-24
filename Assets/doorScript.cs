9ee6d10dacde4007ee22fbb84dce7c717a49a029
Niall McCann <Mc_Cann-N1@ulster.ac.uk>
2025-11-24 11:09:01 +0000

    problem

--- Assets/doorScript.cs
+++ Assets/doorScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class doorScript : MonoBehaviour
 {
     public Transform teleporter1;
-    public Transform Player;
-    Vector3 pos;
+    public GameObject Player;
+    Vector3 pos = Vector3.zero;
     CharacterController characterController;
-
+    Quaternion blank;
+    bool location = false;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,12 +19,29 @@ public class doorScript : MonoBehaviour
 
     void OnTriggerEnter(Collider door)
     {
-        Debug.Log("collision");
+        //Debug.Log("collision");
         if (door.CompareTag("Player"))
         {
+            location = true;
+            teleporter1.transform.GetPositionAndRotation(out pos, out blank);
+            Debug.Log(pos);
+
+            Player.transform.position = teleporter1.position;
             
-            characterController.Move(teleporter1.position);
             Debug.Log("Teleport");
         }
     }
+    void LateUpdate()
+    {
+        if (pos != Vector3.zero)
+        {
+            Player.transform.position = pos;
+            location = false;
+            pos = Vector3.zero;
+            Debug.Log("Teleport");
+        }
+        
+
+    }
+
 }
